0ceb4fbac7efa1dd6931b4b39ad0b66a38f3dd26
mobvebj5 <92158401+mobvebj5@users.noreply.github.com>
2022-01-26 17:59:43 +0330

    Update mobveb.py

--- mobveb.py
+++ mobveb.py
@@ -1,3 +1,7 @@
 print("hello")
 soal = input('esmet chie?')
-print ( soal + 'che esme ghashangi!')
+print ( soal + ' che esme ghashangi!')
+print ("esme manam mohamade")
+dost = input('to bache dary?esmesh chie? ')
+print ( dost + 'khoda hefzesh kone')
+
